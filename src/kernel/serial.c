7cb82ac5e45e4668e726db91500fddfc50cab40b
Selith <selithrubasingha@gmail.com>
2026-06-16 21:12:56 +0530

    feat: serial port configure line method

--- src/kernel/serial.c
+++ src/kernel/serial.c
@@ -40,3 +40,19 @@ void serial_configure_baud_rate(unsigned short com, unsigned short divisor)
     outb(SERIAL_DATA_PORT(com),
          divisor & 0x00FF);
 }
+
+/** serial_configure_line:
+ * Configures the line of the given serial port. The port is set to have a
+ * data length of 8 bits, no parity bits, one stop bit and break control
+ * disabled.
+ *
+ * @param com  The serial port to configure
+ */
+void serial_configure_line(unsigned short com)
+{
+    /* Bit:     | 7 | 6 | 5 4 3 | 2 | 1 0 |
+     * Content: | d | b | prty  | s | dl  |
+     * Value:   | 0 | 0 | 0 0 0 | 0 | 1 1 | = 0x03
+     */
+    outb(SERIAL_LINE_COMMAND_PORT(com), 0x03);
+}
