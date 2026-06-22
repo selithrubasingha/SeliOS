a6e3088a7931114faf731a7699849f840fa396d2
Selith <selithrubasingha@gmail.com>
2026-06-22 22:54:15 +0530

    feat: wrote set entry method

--- src/kernel/gdt.c
+++ src/kernel/gdt.c
@@ -14,3 +14,16 @@ struct gdt_entry {
 
 struct gdt_entry gdt_entries[5]; // Our 5 rules
 struct gdt gdt_ptr;              // The book's pointer struct
+
+void set_entry(int index, unsigned int base, unsigned int limit, unsigned char access, unsigned char gran) {
+    gdt_entries[index].base_low    = (base & 0xFFFF);
+    gdt_entries[index].base_middle = (base >> 16) & 0xFF;
+    gdt_entries[index].base_high   = (base >> 24) & 0xFF;
+    gdt_entries[index].limit_low   = (limit & 0xFFFF);
+    gdt_entries[index].granularity = (limit >> 16) & 0x0F;
+    gdt_entries[index].granularity |= gran & 0xF0;
+    gdt_entries[index].access      = access;
+}
+
+
+
