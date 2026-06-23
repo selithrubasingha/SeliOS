598d5d1735cfd231d2a6a8c421a9d746100f280f
Selith <selithrubasingha@gmail.com>
2026-06-23 14:39:43 +0530

    feat : IDT entry struct

--- src/kernel/idt.h
+++ src/kernel/idt.h
@@ -8,6 +8,15 @@ struct idt {
     unsigned short size;
 } __attribute__((packed));
 
+struct idt_entry {
+    unsigned short base_low;
+    unsigned short sel; // Kernel segment selector
+    unsigned char  always0;
+    unsigned char  flags;
+    unsigned short base_high;
+} __attribute__((packed));
+
+void set_idt_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
 
 void init_idt();
 
