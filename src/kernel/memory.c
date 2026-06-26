c030a3eb57e71179e06471ab742c476ade336f1a
Selith <selithrubasingha@gmail.com>
2026-06-26 15:37:38 +0530

    feat:initializing the page frame allocator via init_memory metho

--- src/kernel/memory.c
+++ src/kernel/memory.c
@@ -11,3 +11,32 @@ void clear_bit(unsigned int index) {
 int test_bit(unsigned int index) {
     return (bitmap[index / 8] & (1 << (index % 8))) != 0;
 }
+// 3. THE NEXT STEP: Setup the Hotel!
+void init_memory(unsigned int total_ram_bytes,unsigned int kernel_physical_start, unsigned int kernel_physical_end) {
+    // Step A: Pessimism. Loop through the whole bitmap and set EVERY bit to 1 (Used)
+    for (unsigned int i = 0; i < BITMAP_SIZE * 8; i++) {
+        set_bit(i);
+    }
+    
+    // Step B: Loop through the available RAM (from GRUB) and set those bits to 0 (Free)
+    unsigned int total_frames = total_ram_bytes / 4096; // 4KB per frame
+    for (unsigned int i = 0; i < total_frames; i++) {
+        clear_bit(i);
+    }
+    
+    
+    // Step C: Protect the VIP! Calculate which frames the Kernel is sitting in, 
+    // and set those specific bits back to 1 (Used)!
+    unsigned int kernel_start_frame = kernel_physical_start / 4096;
+    unsigned int kernel_end_frame = (kernel_physical_end + 4095) / 4096;
+    for (unsigned int i = kernel_start_frame; i <= kernel_end_frame; i++) {
+        set_bit(i);
+    }
+
+    // Protect the first 1MB of RAM!
+    // The first 1MB (Frames 0 to 255) contains the BIOS and your VGA Screen!
+    // If we don't protect it, your OS will hand out the screen memory to apps.
+    for (unsigned int i = 0; i < 256; i++) {
+        set_bit(i);
+    }
+}
