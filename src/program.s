ccb2d3771f2165d6f4f8d22d8a617f78ed6ba353
Selith <selithrubasingha@gmail.com>
2026-07-05 02:10:34 +0530

    feat: assembly start method for the external program

--- src/program.s
+++
@@ -1,6 +0,0 @@
-
-[BITS 32]             ; NEW: Tell NASM to output 32-bit machine code!
-; src/program.s
-mov eax, 0xDEADBEEF   ; A highly recognizable hex number
-;$ means 
-jmp $                 ; Infinite loop
