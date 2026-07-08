210a16a09fbf4db338bdf1ffc3b7abdbd010bc24
Selith <selithrubasingha@gmail.com>
2026-07-08 13:52:34 +0530

    feat: write open and close fs commands

--- src/kernel/fs.c
+++ src/kernel/fs.c
@@ -1,5 +1,5 @@
 # include "fs.h"
-
+#define NULL ((void *)0)
 
 unsigned int read_fs(fs_node_t *node, unsigned int offset, unsigned int size, char *buffer) {
     // Now you can actually use 'node', 'offset', 'size', and 'buffer' in here!
@@ -9,4 +9,25 @@ unsigned int read_fs(fs_node_t *node, unsigned int offset, unsigned int size, ch
 
     return 0; // Return 0 if the read function is not defined   
 
+}
+
+unsigned int write_fs(fs_node_t *node, unsigned int offset, unsigned int size, char *buffer) {
+    if (node->write) {
+        return node->write(node, offset, size, buffer);
+    }
+
+    return 0; // Return 0 if the write function is not defined
+}
+
+// open fs and closed fs
+void open_fs(fs_node_t *node) {
+    if (node->open) {
+        node->open(node);
+    }
+}
+
+void close_fs(fs_node_t *node) {
+    if (node->close) {
+        node->close(node);
+    }
 }
