53b0f0172b75edeed408ae0e268cdccf895b18c0
Selith <selithrubasingha@gmail.com>
2026-07-10 23:42:04 +0530

    feat: activated interrupts in usermode.s

--- src/kernel/user_mode.s
+++ src/kernel/user_mode.s
@@ -11,7 +11,11 @@ enter_user_mode:
     ; 2. Forge the IRET stack (pushed in reverse order)
     push 0x23         ; SS: Stack Segment 
     push 0xBFFFFFFB   ; ESP: Stack Pointer
-    push 0x2          ; EFLAGS: Interrupts disabled
+    push 0x202          ; EFLAGS: Interrupts disabled
+    ;if your 9th bit is a 0, which means the moment you jump to Ring 3, the CPU plugs its ears and ignores all interrupts 
+    ;(including your keyboard timer and your new 0x80 system call).
+    
+    
     push 0x1B         ; CS: Code Segment (0x18 | 0x3)
     push 0x00000000   ; EIP: Instruction Pointer
 
