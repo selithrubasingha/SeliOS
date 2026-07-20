ddaff710713e2f9e679211711fd7835a410cc26c
Selith <selithrubasingha@gmail.com>
2026-07-20 23:44:21 +0530

    feat: updates on selifetch and help commands

--- src/kernel/terminal.c
+++ src/kernel/terminal.c
@@ -100,13 +100,17 @@ void terminal_handle_scan(unsigned char scancode) {
 void terminal_run_command(const char *cmd) {
     if (strcmp(cmd, "help") == 0) {
         printf(DEVICE_FB, "Available commands:\n");
-        printf(DEVICE_FB, "  \x0Bhelp\x07      - Display this command dictionary.\n");
-        printf(DEVICE_FB, "  \x0B" "clear\x07     - Clear the VGA display buffer.\n");
-        printf(DEVICE_FB, "  \x0B" "whoami\x07    - Reveal current system privileges.\n");
-        printf(DEVICE_FB, "  \x0B" "selifetch\x07 - Output modern system architecture stats.\n");
-        printf(DEVICE_FB, "  \x0B" "memory\x07    - Display physical RAM and Kernel footprint.\n");
-    } 
-    else if (strcmp(cmd, "clear") == 0) {
+        printf(DEVICE_FB, "  \x0B" "help\x07        - Display this command dictionary.\n");
+        printf(DEVICE_FB, "  \x0B" "clear\x07       - Clear the VGA display buffer.\n");
+        printf(DEVICE_FB, "  \x0B" "whoami\x07      - Reveal current system privileges.\n");
+        printf(DEVICE_FB, "  \x0B" "selifetch\x07   - Output modern system architecture stats.\n");
+        printf(DEVICE_FB, "  \x0B" "memory\x07      - Display physical RAM and Kernel footprint.\n");
+        printf(DEVICE_FB, "  \x0B" "ls\x07          - Scan and list files in the virtual RAM disk.\n");
+        printf(DEVICE_FB, "  \x0B" "cat <file>\x07  - Read and dump a file's payload to screen.\n");
+        printf(DEVICE_FB, "  \x0B" "matrix\x07      - Launch the preemptive concurrency engine.\n");
+    }
+    
+   else if (strcmp(cmd, "clear") == 0) {
         clear_screen();
     } 
     else if (strcmp(cmd, "whoami") == 0) {
@@ -124,10 +128,10 @@ void terminal_run_command(const char *cmd) {
         printf(DEVICE_FB, " Bytes\n\n");
     }
     else if (strcmp(cmd, "selifetch") == 0) {
-        printf(DEVICE_FB, "\x0A   ____       _ _  ___  ____   \x07 Host: Lenovo Legion Pro 5\n");
-        printf(DEVICE_FB, "\x0A  / ___|  ___| (_)/ _ \\/ ___|  \x07 OS:   SeliOS x86 Microkernel\n");
-        printf(DEVICE_FB, "\x0A  \\___ \\ / _ \\ | | | | \\___ \\  \x07 Architecture: i686-elf\n");
-        printf(DEVICE_FB, "\x0A   ___) |  __/ | | |_| |___) | \x07 Target Environment: Ring 0 Kernel\n");
+        printf(DEVICE_FB, "\x0A   ____       _ _  ___  ____   \x07 Host: Lenovo Legion Pro 5");
+        printf(DEVICE_FB, "\x0A  / ___|  ___| (_)/ _ \\/ ___|  \x07 OS:   SeliOS x86 Microkernel");
+        printf(DEVICE_FB, "\x0A  \\___ \\ / _ \\ | | | | \\___ \\  \x07 Architecture: i686-elf");
+        printf(DEVICE_FB, "\x0A   ___) |  __/ | | |_| |___) | \x07 Target Environment: Ring 0 Kernel");
         printf(DEVICE_FB, "\x0A  |____/ \\___|_|_|\\___/|____/  \x07 UI Framework: Text Mode 80x25\n\n");
     }
     else if (strcmp(cmd, "ls") == 0) {
